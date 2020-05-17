36613b9102b32e396c59ab4c6cef525280e842e1
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-05-17 20:35:36 +1000

    feat: Fetch list of latest videos
    Fetch them from the Giantbomb API

--- giantbomb/ContentView.swift
+++ giantbomb/ContentView.swift
@@ -8,11 +8,66 @@
 
 import SwiftUI
 
+struct Video : Decodable, Identifiable {
+    var id: Int
+    var guid: String
+    var name: String
+    var publishDate: String
+    var premium: Bool
+    var lengthSeconds: Int
+}
+
+struct Videos : Decodable {
+    var results: [Video]
+}
+
+class FetchVideos: ObservableObject {
+    @Published var videos: Videos
+
+    init() {
+        self.videos = Videos(results: [] as [Video])
+
+        var thing = URLComponents(string: Environment.gbApiUrl)
+        let path = "/api/videos"
+        thing?.path = path
+        thing?.queryItems = [
+            URLQueryItem(name: "api_key", value: Environment.gbApiKey),
+            URLQueryItem(name: "sort", value: "publish_date:desc"),
+            URLQueryItem(name: "format", value: "json")
+        ]
+        guard let url = thing?.url else { return }
+
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            do {
+                if let videosData = data {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let decodedData = try decoder.decode(Videos.self, from: videosData)
+                    DispatchQueue.main.async {
+                        self.videos = decodedData
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print("Error")
+            }
+        }.resume()
+    }
+}
+
+
 struct ContentView: View {
+    @ObservedObject var fetchVideos = FetchVideos()
+
     var body: some View {
         VStack {
-            Text(Environment.gbApiKey) 
-            Text(Environment.gbApiUrl.absoluteString)
+            List(fetchVideos.videos.results) { video in
+                HStack(alignment: .top) {
+                    Text(video.premium ? "[P]" : "")
+                    Text(video.name)
+                }
+            }
         }
     }
 }
